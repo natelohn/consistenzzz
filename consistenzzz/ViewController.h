42ff35bf536d8baecd6410cad56e4245d4bddbf0
natelohn <nlohn@stanford.edu>
2015-11-15 18:22:24 -0800

    Initial Commit

---
+++ consistenzzz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  consistenzzz
+//
+//  Created by Nate Lohn on 11/15/15.
+//  Copyright © 2015 Nate Lohn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ consistenzzz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  consistenzzz
+//
+//  Created by Nate Lohn on 11/15/15.
+//  Copyright © 2015 Nate Lohn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
